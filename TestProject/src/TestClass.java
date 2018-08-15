e7a43470ff5e2f2ac3854ddd445c3a41ba2a80b0
Dinesh <Dinesh@lt-chn04>
2018-08-15 13:29:39 +0530

    Second change

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -3,7 +3,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("My First git commit");
-
+		System.out.println("Checking it again");
+		System.out.println("do it again");
 	}
 
 }
